91efa0a1dc57436a6bf542cf5c2872692fa69417
benten <ben@benten.ca>
2022-05-20 14:29:30 -0600

    config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'handwriting': ['Kalam', 'cursive'],
+        'fancy': ['Spirax', 'cursive']
+      },
+    },
+  },
+  plugins: [],
+}
