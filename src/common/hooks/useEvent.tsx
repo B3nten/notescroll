0e9e6ea0a2dc7457303b897c2cdd6bbb96119ffc
benten <ben@benten.ca>
2022-06-01 20:26:56 -0600

    created useEvent hook

---
+++ src/common/hooks/useEvent.tsx
@@ -0,0 +1,14 @@
+import { useLayoutEffect, useRef, useCallback } from "react";
+
+export function useEvent(handler:()=>any) {
+    const handlerRef = useRef<any>(null);
+  
+    useLayoutEffect(() => {
+      handlerRef.current = handler;
+    });
+  
+    return useCallback((...args:any) => {
+      const fn = handlerRef.current;
+      return fn(...args);
+    }, []);
+  }
