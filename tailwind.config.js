7bf73b64013b69f6289a2b3aedfe1e2940b52a30
benten <ben@benten.ca>
2022-06-10 12:47:57 -0600

    updated theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,35 +27,6 @@ module.exports = {
 
   daisyui: {
     base: true,
-    darkTheme:'default',
-    themes: [
-      {
-        default: {
-          primary: "#ef9995",
-          "primary-content": "#282425",
-          secondary: "#a4cbb4",
-          "secondary-content": "#282425",
-          accent: "#ebdc99",
-          "accent-content": "#282425",
-          neutral: "#7d7259",
-          "neutral-content": "#e4d8b4",
-          "base-100": "#e4d8b4",
-          "base-200": "#d2c59d",
-          "base-300": "#c6b386",
-          "base-content": "#282425",
-          "info": "#2563eb",
-          "success": "#16a34a",
-          "warning": "#d97706",
-          "error": "#dc2626",
-          "--rounded-box": "0.4rem",
-          "--rounded-btn": "0.4rem",
-          "--rounded-badge": "0.4rem",
-        },
-      },
-      'dark',
-      'lemonade',
-      'autumn',
-      'retro',
-    ],
+    darkTheme: 'dark',
   },
 }
