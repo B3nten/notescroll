560760ec05a519916e929821f601b12d569ff684
benten <ben@benten.ca>
2022-06-10 12:49:34 -0600

    added queries

--- src/common/hooks/useClientRouter.tsx
+++ src/common/hooks/useClientRouter.tsx
@@ -1,25 +1,30 @@
 import { match } from 'path-to-regexp'
-import router, { makePublicRouterInstance, useRouter } from 'next/router';
+import router, { makePublicRouterInstance, useRouter } from 'next/router'
 
-import type { Router, NextRouter } from 'next/router';
-import type { MatchResult } from 'path-to-regexp';
-import type { ParsedUrlQuery } from 'querystring';
+import type { Router, NextRouter } from 'next/router'
+import type { MatchResult } from 'path-to-regexp'
+import type { ParsedUrlQuery } from 'querystring'
 
-function fixClientRouter() {
-    if (typeof window !== 'undefined') {
-      const windowPathname = window.location.pathname
-      const input = router.pathname.replaceAll('[',':').replaceAll(']','')
-      return {
-        ...makePublicRouterInstance(router.router as Router),
-        pathname: windowPathname,
-        query: (match(input)(window.location.pathname) as MatchResult<ParsedUrlQuery>).params
-      }
-    } else {
-      return { query: {}, pathname: '', route: '', asPath: '', basePath: '' }
+
+function fixClientRouter(): NextRouter {
+  if (typeof window !== 'undefined') {
+    const windowPathname = window.location.pathname
+    const input = router.pathname.replaceAll('[', ':').replaceAll(']', '')
+    let queries: {} = {}
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.forEach((value, key) => queries[key] = value)
+
+    return {
+      ...makePublicRouterInstance(router.router as Router),
+      pathname: windowPathname,
+      query: { ...(match(input)(window.location.pathname) as MatchResult<ParsedUrlQuery>).params, ...queries }
     }
+  } else {
+    return { query: {}, pathname: '', route: '', asPath: '', basePath: '' } as NextRouter
   }
-  
-  export function useClientRouter(){
-    useRouter()
-    return fixClientRouter()
-  }
+}
+
+export function useClientRouter(): NextRouter {
+  useRouter()
+  return fixClientRouter()
+}
